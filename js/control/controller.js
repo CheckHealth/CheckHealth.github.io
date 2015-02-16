19f63c45228542b3c7d5ff4f1db252df645fae82
Lorenzo Di Tucci <lorenzo.ditucci@mail.polimi.it>
2015-02-15 20:19:37 -0600

    added combination of color for the heatmap

--- js/control/controller.js
+++ js/control/controller.js
@@ -35,10 +35,10 @@ Controller.prototype.testD3TopoJson= function(){
 
         var color = d3.scale.threshold()
             .domain([1, 10, 50, 100, 500, 1000, 2000, 5000])
-            .range(['#ffffd9', '#edf8b1', '#c7e9b4', '#7fcdbb', '#41b6c4', '#1d91c0', '#225ea8', '#253494', '#081d58']);
+            //.range(['#ffffd9', '#edf8b1', '#c7e9b4', '#7fcdbb', '#41b6c4', '#1d91c0', '#225ea8', '#253494', '#081d58']);
             //.range(["#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84", "#fc8d59", "#ef6548", "#d7301f", "#b30000", "#7f0000"]);
             //.range(['#f7fcfd', '#e0ecf4', '#bfd3e6', '#9ebcda', '#8c96c6', '#8c6bb1', '#88419d', '#810f7c', '#4d004b']);
-
+            .range(['rgb(255,255,204)','rgb(255,237,160)','rgb(254,217,118)','rgb(254,178,76)','rgb(253,141,60)','rgb(252,78,42)','rgb(227,26,28)','rgb(189,0,38)','rgb(128,0,38)']);
 
 
         // var color = d3.scale.threshold()
